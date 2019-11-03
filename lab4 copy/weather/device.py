fed81b07e5f2e18576fa7ccc602d9c361d57baac
Robert <gusroberth@student.gu.se>
2019-11-03 14:29:45 +0100

    up

--- lab4 copy/weather/device.py
+++ lab4 copy/weather/device.py
@@ -14,14 +14,9 @@ class WeatherDevice(DddDevice):
 	    return temperature
         '''
 
-    class temperature(DeviceWHQuery):
-        def perform(self,select_city, select_units, select_country):
-            url = 'http://api.openweathermap.org/data/2.5/city?q=%s&country?q=%s$units?q=%s&APPID=765e517f55ddd638e72b43b675f71621' % (select_city,select_country, select_units)
-            print url
-            request = Request(url)
-            response = urlopen(request)
-            data = response.read()
-            return json.loads(data)
+    class account_details(DeviceWHQuery):
+        def perform(self,select_account):
+            print "poop"
 
 
     CARDIFF  = "city_cardiff"
