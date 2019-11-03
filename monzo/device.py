22e8768fb3031880f278446a3e524a9be084b28c
Robert <gusroberth@student.gu.se>
2019-11-03 20:17:56 +0100

    i think we're nearly there!

--- monzo/device.py
+++ monzo/device.py
@@ -24,17 +24,17 @@ class MonzoDevice(DddDevice):
             response = clean_account_details(requests.get(endpoint, headers=headers).json())
             return response
 
-CURRENT = "user_00009Z2sSGIUDzcXctzkcD"
-BALANCE = "balance"
-TRANSACTIONS = "transactions"
-POTS = "pots"
-ACCOUNTS = "https://api.monzo.com/balance"
+    CURRENT = "user_00009Z2sSGIUDzcXctzkcD"
+    BALANCE = "balance"
+    TRANSACTIONS = "transactions"
+    POTS = "pots"
+    ACCOUNTS = "https://api.monzo.com/balance"
 
-HEADERS = {"Authorization" : "Bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJlYiI6IlFXUi93TER6QlVYZGs2Vk5iNTZ3IiwianRpIjoiYWNjdG9rXzAwMDA5b2E3bU81bFlla0wyTldlM3QiLCJ0eXAiOiJhdCIsInYiOiI2In0.nd2c5dnhCOrWgPMOycHsWMaIaHnpq6DUpmvo9QJ6fCTjPjrytKT8v09PripFNZFtBXTwHlRTxcdRUZ3Do-sgeA"}
-ACCOUNT_ID = "user_00009Z2sSGIUDzcXctzkcD"
-POT_ID = "pot_00009cj4TafFU2lqI95qR0"
+    HEADERS = {"Authorization" : "Bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJlYiI6IlFXUi93TER6QlVYZGs2Vk5iNTZ3IiwianRpIjoiYWNjdG9rXzAwMDA5b2E3bU81bFlla0wyTldlM3QiLCJ0eXAiOiJhdCIsInYiOiI2In0.nd2c5dnhCOrWgPMOycHsWMaIaHnpq6DUpmvo9QJ6fCTjPjrytKT8v09PripFNZFtBXTwHlRTxcdRUZ3Do-sgeA"}
+    ACCOUNT_ID = "user_00009Z2sSGIUDzcXctzkcD"
+    POT_ID = "pot_00009cj4TafFU2lqI95qR0"
 
-API_ENDPOINTS = {BALANCE     : "https://api.monzo.com/balance",
+    API_ENDPOINTS = {BALANCE     : "https://api.monzo.com/balance",
                  #Shows the user their acount balance
                  ACCOUNTS    : "https://api.monzo.com/accounts",
                  #Shows the user account information
