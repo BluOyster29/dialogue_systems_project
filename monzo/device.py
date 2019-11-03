4e4dc9cf4ba0a4b9e80ccb57a942bbd5c489009a
Robert <gusroberth@student.gu.se>
2019-11-03 12:52:51 +0100

    updating

--- monzo/device.py
+++ monzo/device.py
@@ -21,9 +21,12 @@ CURRENT = "user_00009Z2sSGIUDzcXctzkcD"
 BALANCE = "balance"
 TRANSACTIONS = "transactions"
 POTS = "pots"
+ACCOUNTS = "https://api.monzo.com/balance"
+
 HEADERS = {"Authorization" : "Bearer eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJlYiI6IlFXUi93TER6QlVYZGs2Vk5iNTZ3IiwianRpIjoiYWNjdG9rXzAwMDA5b2E3bU81bFlla0wyTldlM3QiLCJ0eXAiOiJhdCIsInYiOiI2In0.nd2c5dnhCOrWgPMOycHsWMaIaHnpq6DUpmvo9QJ6fCTjPjrytKT8v09PripFNZFtBXTwHlRTxcdRUZ3Do-sgeA"}
 ACCOUNT_ID = "user_00009Z2sSGIUDzcXctzkcD"
 POT_ID = "pot_00009cj4TafFU2lqI95qR0"
+
 API_ENDPOINTS = {BALANCE     : "https://api.monzo.com/balance",
                  #Shows the user their acount balance
                  ACCOUNTS    : "https://api.monzo.com/accounts",
